c68fb4c7bb5fc9c4ea67f8281794efd47e203152
Tommy McDonald <tommydanielmcdonald@gmail.com>
2017-11-28 22:26:42 -0500

    Update

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
 app.get('/test', (req, res) => {
    res.send('working');
