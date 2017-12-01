f4a32ade63d954a4cbec0cf3c5d14bfb43365e8c
adottino <anthonydottino@gmail.com>
2017-12-01 14:33:26 -0500

    changes

--- database.js
+++ database.js
@@ -1,12 +1,19 @@
-const mysql = require('mysql');
+var mysql = require('mysql');
 
-const con = mysql.createConnection({
-  host: "localhost",
-  user: "yourusername",
-  password: "yourpassword"
+var con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'nonenone',
+  database: 'RTC'
 });
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
+  /*var sql = "CREATE TABLE Songs (name VARCHAR(255), genre VARCHAR(255), rank INT, playcount INT)";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });*/
+  //var sql2 = "CREATE TABLE Artists (name VARCHAR(255), genre VARCHAR(255), rank INT)";
 });
