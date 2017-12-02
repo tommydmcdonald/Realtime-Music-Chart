fd318b32c06fc811a143166cb018d704aaef5919
adottino <anthonydottino@gmail.com>
2017-12-01 22:22:17 -0500

    sql tables created

--- database.js
+++ database.js
@@ -10,10 +10,24 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  /*var sql = "CREATE TABLE Songs (name VARCHAR(255), genre VARCHAR(255), rank INT, playcount INT)";
+var sql = "CREATE TABLE IF NOT EXISTS Song (rank INT PRIMARY KEY, song_name VARCHAR(255), url VARCHAR(255))";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table created");
-  });*/
-  //var sql2 = "CREATE TABLE Artists (name VARCHAR(255), genre VARCHAR(255), rank INT)";
+  });
+  var sql = "CREATE TABLE IF NOT EXISTS Artist (artist_name VARCHAR(255) PRIMARY KEY)";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+  var sql = "CREATE TABLE IF NOT EXISTS Stream_count (rank INT, stream_count INT, FOREIGN KEY(rank) REFERENCES Song(rank) ON UPDATE CASCADE)";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+  var sql = "CREATE TABLE IF NOT EXISTS Songs_artist (rank INT, artist_name VARCHAR(255), FOREIGN KEY(rank) REFERENCES Songs(rank) ON UPDATE CASCADE)";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
 });
