c6c54a5a41d7037bcd9a98f5673b652264e121b6
Tommy McDonald <tommydanielmcdonald@gmail.com>
2017-12-01 22:53:13 -0500

    Added database adding and creating

--- database.js
+++ database.js
@@ -4,34 +4,41 @@ const con = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'nonenone',
-  database: 'RTC'
 });
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
   var sql = "CREATE DATABASE IF NOT EXISTS RTC";
-  con.query(sql, function (err, result) {
+
+  const conDB = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'nonenone',
+    database: 'RTC'
+  });
+
+  conDB.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Database created");
   });
 var sql = "CREATE TABLE IF NOT EXISTS Song (rank INT PRIMARY KEY, song_name VARCHAR(255), url VARCHAR(255))";
-  con.query(sql, function (err, result) {
+  conDB.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table created");
   });
   var sql = "CREATE TABLE IF NOT EXISTS Artist (artist_name VARCHAR(255) PRIMARY KEY)";
-  con.query(sql, function (err, result) {
+  conDB.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table created");
   });
   var sql = "CREATE TABLE IF NOT EXISTS Stream_count (rank INT, stream_count INT, FOREIGN KEY(rank) REFERENCES Song(rank) ON UPDATE CASCADE)";
-  con.query(sql, function (err, result) {
+  conDB.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table created");
   });
   var sql = "CREATE TABLE IF NOT EXISTS Songs_artist (rank INT, artist_name VARCHAR(255), FOREIGN KEY(rank) REFERENCES Song(rank) ON UPDATE CASCADE)";
-  con.query(sql, function (err, result) {
+  conDB.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table created");
   });
