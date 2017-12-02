2d9d91ab8bf7c669c3dae87988a6b3770f3feab3
adottino <anthonydottino@gmail.com>
2017-12-01 22:45:58 -0500

    now it works

--- database.js
+++ database.js
@@ -3,13 +3,18 @@ const mysql = require('mysql');
 const con = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'jpyfT_!=k6Ch',
-  database: 'rtc'
+  password: 'nonenone',
+  database: 'RTC'
 });
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
+  var sql = "CREATE DATABASE IF NOT EXISTS RTC";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+  });
 var sql = "CREATE TABLE IF NOT EXISTS Song (rank INT PRIMARY KEY, song_name VARCHAR(255), url VARCHAR(255))";
   con.query(sql, function (err, result) {
     if (err) throw err;
@@ -25,7 +30,7 @@ var sql = "CREATE TABLE IF NOT EXISTS Song (rank INT PRIMARY KEY, song_name VARC
     if (err) throw err;
     console.log("Table created");
   });
-  var sql = "CREATE TABLE IF NOT EXISTS Songs_artist (rank INT, artist_name VARCHAR(255), FOREIGN KEY(rank) REFERENCES Songs(rank) ON UPDATE CASCADE)";
+  var sql = "CREATE TABLE IF NOT EXISTS Songs_artist (rank INT, artist_name INT, FOREIGN KEY(rank) REFERENCES Song(rank) ON UPDATE CASCADE)";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table created");
